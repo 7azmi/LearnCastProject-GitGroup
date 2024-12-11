f305aefeae04216e358c4c2b64f8a525f8b3c7b3
7azmi <a7.h3.04@gmail.com>
2024-12-11 09:42:18 +0800

    init with some front end pages

--- src/main/java/org/example/LearnCastProjectApplication.java
+++ src/main/java/org/example/LearnCastProjectApplication.java
@@ -1,11 +1,11 @@
-package org.example;
+package org.example; // Update based on your actual package structure
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class LearnCastProjectApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(LearnCastProjectApplication.class, args);
     }
 }
